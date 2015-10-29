8dc611ffed276bfc54e8b12cec1b474dac023580
Graham Barry <grahambarry@live.ie>
2015-10-29 22:59:44 +0000

    Password fix3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,8 +78,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://limitless-dawn-1982.herokuapp.com'
-  config.action_mailer.default_url_options = { host:, 'https://limitless-dawn-1982.herokuapp.com' }
+  host = 'limitless-dawn-1982.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
