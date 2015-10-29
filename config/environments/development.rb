41f1b5898315dc9d36ef3340cf9164973a7393f1
Graham Barry <grahambarry@live.ie>
2015-10-29 20:33:47 +0000

    finally corrected productionrb for AMAZON and PaperClip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,5 +44,16 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: host }
 
 
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['artrisings3'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    },
+    :url => ':s3_domain_url',
+    :path => "/:class/:attachment/:id_partition/:style/:filename"
+  }
+
 
 end
